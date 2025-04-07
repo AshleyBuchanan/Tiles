423252cf35270819bab6806219b4af1331f3bd2c
AshleyBuchanan <ashleyshanebuchanan@gmail.com>
2025-04-07 10:59:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { prototype } = require('events');
+const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
+const app = express();
+const host = 'localhost';
+const port = 3000;
+
+app.use(methodOverride('__method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`listening on ${host}:${port}.`);
+});
